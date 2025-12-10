b1ea84c9a896993169bb8b0afd1ddf92890146f3
shayan17shane19 <shayansetayandevolleyking5@gmail.com>
2025-12-11 00:54:51 +0330

    map and copy data

--- test.cpp
+++ test.cpp
@@ -7,31 +7,18 @@ using namespace std;
 
 int main()
 {
-    const float dt = 0.1;
+    double data[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
-    // state transition matrix
-    MatrixXd A(4, 4);
-    A.setIdentity();
-    A(0, 2) = dt;
-    A(1, 3) = dt;
+    Map<Vector4d> current_state(data);
+    Map<Vector2d> current_input(data + 4);
+    Map<Vector4d> ref_state(data + 6);
 
-    // input matrix
-    MatrixXd B(4, 2);
-    B(0, 0) = pow(dt, 2) / 2;
-    B(1, 1) = pow(dt, 2) / 2;
-    B(2, 0) = dt;
-    B(3, 1) = dt;
+    Vector2d current_position = current_state.head<2>();
+    Vector2d ref_position = ref_state.head<2>();
 
-    // current state
-    MatrixXd x_k(4, 1);
-    x_k << 1.0, 2.0, 0.5, 0.1;
+    Vector2d error = ref_position - current_position;
 
-    // current input
-    MatrixXd u_k(2, 1);
-    u_k << 0.2, -0.1;
+    cout << error << endl;
 
-    // next state x(k + 1) = A x(k) + B u(k)
-    Vector4d x_k1 = A * x_k + B * u_k;
-    cout << x_k1 << endl;
     return 0;
 }
