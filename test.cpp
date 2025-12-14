2af7b399d0a57f2d2e85f12b6b7daa5216f554a0
shayan17shane19 <shayansetayandevolleyking5@gmail.com>
2025-12-14 14:39:53 +0330

    compare two method of multiple

--- test.cpp
+++ test.cpp
@@ -1,27 +1,39 @@
 #include <iostream>
+#include <chrono>
 #include <eigen3/Eigen/Dense>
 // #include <eigen3/Eigen/Sparse>
 
 using namespace Eigen;
 using namespace std;
+using namespace chrono;
 
 int main()
 {
-    Matrix3d H(3, 3);
-    H << 4, 1, 1,
-        1, 5, 1,
-        1, 1, 3;
+    MatrixXd a(10, 10);
+    MatrixXd b(10, 10);
+    a.setIdentity();
+    b.setIdentity();
 
-    Vector3d G = {1, 2, 3};
+    auto start_2 = high_resolution_clock::now();
+    MatrixXd d(10, 10);
+    d.setIdentity();
+    cout << d << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            d(i, j) = a(i, j) * b(i, j);
+        }
+    }
+    auto end_2 = high_resolution_clock::now();
+    auto time_2 = duration_cast<microseconds>(end_2 - start_2).count();
+    cout << "second time : " << time_2 << endl;
 
-    LDLT<Matrix3d> solver;
-    solver.compute(H);
-    Vector3d X = solver.solve(G);
-
-    cout << X << endl;
-
-    Vector3d error = H * X - G;
-    cout << error << endl;
+    auto start_3 = high_resolution_clock::now();
+    MatrixXd e = (a.array() * b.array()).matrix();
+    auto end_3 = high_resolution_clock::now();
+    auto time_3 = duration_cast<microseconds>(end_3 - start_3).count();
+    cout << "third time : " << time_3 << endl;
 
     return 0;
 }
